ca0e1a215ed112dd8a1394017640b4695c5d6c3f
HuanCraven <sybrizrael@gmail.com>
2026-08-01 15:32:55 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kaltrack-v26';
+const CACHE = 'kaltrack-v27';
 const SHELL = ['./', './index.html', './manifest.json', './zxing.js', './zaklad.js'];
 
 self.addEventListener('install', e => {
