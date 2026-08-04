e22c1238dfaede9c6bd048197dfe1a347186b560
Huan <sybrizrael@gmail.com>
2026-08-04 18:02:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kaltrack-v48';
+const CACHE = 'kaltrack-v49';
 const SHELL = ['./', './index.html', './manifest.json', './zxing.js', './zaklad.js', './jidla.js'];
 
 self.addEventListener('install', e => {
